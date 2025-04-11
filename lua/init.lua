35b70e07dbc868517ae521fc72484041339c5d0e
Stojanov <stojanovangel@outlook.com>
2025-04-11 17:06:49 +0200

    Added folding

--- lua/init.lua
+++ lua/init.lua
@@ -62,6 +62,10 @@ g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldlevelstart = 99  -- Set initial fold level (99 = open all, 0 = close all)
+
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.fn.has "win32" ~= 0
 local sep = is_windows and "\\" or "/"
